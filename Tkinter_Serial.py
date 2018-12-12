6a6dfa7bf32dc4dd116d98624ecdbecc1598fd7c
Prabanj <45552840+Prabanj@users.noreply.github.com>
2018-12-12 17:14:17 +0530

    Add files via upload

--- Tkinter_Serial.py
+++ Tkinter_Serial.py
@@ -7,6 +7,8 @@ Created on Tue Nov 20 16:06:01 2018
 
 import serial 
 import pyperclip
+import Tkinter
+from Tkinter import *
 
 
 # if you are still working under a Python 2 version, 
@@ -16,9 +18,9 @@ import pyperclip
 
 
 
-ser = serial.Serial()
-ser.port = 'COM8'
-ser.open()
+s = serial.Serial()
+s.port = 'COM8'
+s.open()
 #while True:
 #    a = ser.readline() 
 #    print(int(a[1:18]))
@@ -33,8 +35,28 @@ ser.open()
 #pyperclip.copy(int(a[2:16]))
 
 while True:
-    a = ser.readline() 
+    a = s.readline() 
     print(a)
+    
 #            
 #    #self.labelVariable2.set(int(a[1:18]))
     #pyperclip.copy(a)
+    
+    
+    import serial
+
+#s = serial.Serial('COM10',9600)    # open serial port
+master = Tk()
+master.geometry("1360x750")        # a window pop up with width (1360) and height(750)     which exatly fits my monitor screen..
+
+while 1:
+if s.inWaiting():
+text = s.readline(s.inWaiting())
+frameLabel = Frame( master, padx=40, pady =40)
+frameLabel.pack()
+w = Text( frameLabel, wrap='word', font="TimesNewRoman 37")
+w.insert(12.0,text )
+w.pack()
+w.configure( bg=master.cget('bg'), relief='flat', state='Normal' )
+
+mainloop()
