c92a79655973afd253c37ce3cef4b60512e76bb4
Prabanj <45552840+Prabanj@users.noreply.github.com>
2019-02-14 16:25:34 +0530

    Update

--- OpenMv with tick mark/OpenMv_Find_datamatrices_Tick_Final.py
+++ OpenMv with tick mark/OpenMv_Find_datamatrices_Tick_Final.py
@@ -1,56 +1,69 @@
-# Find Data Matrices w/ Lens Zoom Example
-#
-# This example shows off how easy it is to detect data matrices using the
-# OpenMV Cam M7. Data matrices detection does not work on the M4 Camera.
-
-import sensor, image, time, math
-from pyb import UART
-from pyb import LED
-
+# Untitled - By: CTS-TIH 1 - Tue Dec 11 2018
 
 import pyb
-
-red_led   = LED(2)
-
-
-
-sensor.reset()
-sensor.set_pixformat(sensor.GRAYSCALE)
-sensor.set_framesize(sensor.VGA)
-sensor.set_windowing((320, 240)) # 2x Zoom
-sensor.skip_frames(time = 2000)
-sensor.set_auto_gain(False)  # must turn this off to prevent image washout...
-sensor.set_auto_whitebal(False)  # must turn this off to prevent image washout...
-clock = time.clock()
-
-thresholds = (220, 255)
+from pyb import Pin
+import sensor, image, time, math ,lcd
+from pyb import LED
+blue_led   = LED(3)
+switch = Pin('P4', Pin.IN, Pin.PULL_NONE)
+led = Pin('P9', Pin.OUT_PP, Pin.PULL_NONE)
 
 
 while(True):
-    clock.tick()
-    img = sensor.snapshot()
-    #img.lens_corr(1.8) # strength of 1.8 is good for the 2.8mm lens.
-    #img.mean(1, threshold=True, offset=15, invert=True)
 
-    #img = sensor.snapshot().binary([thresholds], invert=False, zero=True)
+  if(switch.value()):
+      led.high()
+
+      sensor.reset()
+      sensor.set_pixformat(sensor.RGB565)
+      sensor.set_framesize(sensor.VGA)
+
+      #lcd.set_backlight(True)
+      sensor.set_windowing((200, 260)) # 2x Zoom
+      sensor.skip_frames(time = 2000)
+
+      sensor.set_auto_gain(False)  # must turn this off to prevent image washout...
+      sensor.set_auto_whitebal(False)  # must turn this off to prevent image washout...
+
+      #clock = time.clock()
+      lcd.init()
+      while(switch.value()):
+           #clock.tick()
+           img = sensor.snapshot()
+
+           matrices = img.find_datamatrices()
+           for matrix in matrices:
+               img.draw_rectangle(matrix.rect(), color = (255, 0, 0))
+               myImage= image.Image("indication.ppm", copy_to_fb = True)
+               lcd.display(myImage)
+               pyb.delay(400)
+               print(matrix.payload())
+
+           if not matrices:
+               qcode=img.find_qrcodes();
+               for code in qcode:
+                       img.draw_rectangle(code.rect(), color = (255, 0, 0))
+                       myImage= image.Image("indication.ppm", copy_to_fb = True)
+                       lcd.display(myImage)
+                       pyb.delay(400)
+                       print(code.payload())
+
+               if not qcode:
+                 bcode=img.find_barcodes();
+                 for code in bcode:
+                       img.draw_rectangle(code.rect(), color = (255, 0, 0))
+                       myImage= image.Image("indication.ppm", copy_to_fb = True)
+                       lcd.display(myImage)
+                       pyb.delay(400)
+                       print(code.payload())
+
+           lcd.display(img) # Take a picture and display the image.s
 
 
-    matrices = img.find_datamatrices()
-    for matrix in matrices:
-        img.draw_rectangle(matrix.rect(), color = (255, 0, 0))
-        #image.load_decriptor("E:\Intern'18_Sanj\Python Prog\Untitled.jpg")
-        myImage = image.Image("download.ppm", copy_to_fb = True)
-        #print(myImage)
-        print_args = (matrix.payload())
-        print(print_args)
-        red_led.on()
-        pyb.delay(500)
-        red_led.off()
-        pyb.delay(500)
-        #uart = UART(3, 9600)
-        #uart.write('1')
-        #print(matrix.payload())
-        #red_led.on()
-        #pyb.delay(500)
-        #red_led.off()
-        #pyb.delay(2000)
+
+
+  else:
+      led.low()
+      lcd.set_backlight(False)
+      #lcd.deinit()
+      #sensor.sleep(True)
